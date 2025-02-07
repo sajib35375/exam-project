9c32537c5fd2bcd3d3cae95777da9413cc60ca4d
sajib35375 <sajibsust99@gmail.com>
2025-02-07 09:26:29 +0600

    fav icon add

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="{{ asset('frontend/images/fav.png') }}">
     <title>Merchant Dashboard Login</title>
     <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
