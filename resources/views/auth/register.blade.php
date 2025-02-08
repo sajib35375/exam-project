a95569671b12af79c396699f571bb2d6a227785d
sajib35375 <sajibsust99@gmail.com>
2025-02-09 02:26:03 +0600

    register page name fixed

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" type="image/x-icon" href="{{ asset('frontend/images/fav.png') }}">
-    <title>Merchant Dashboard Login</title>
+    <title>Merchant Dashboard Register</title>
     <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 </head>
